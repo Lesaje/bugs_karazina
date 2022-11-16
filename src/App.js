a5a1fb4a7e20a3b89c0e97f7ee16e330b2c7d776
Lesaje <olehchaika.prop@gmail.com>
2022-11-16 23:33:55 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Days with bug: 17</h1>
+        <h2>(since 30.10.2022)</h2>
       </header>
     </div>
   );
