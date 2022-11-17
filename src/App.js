533389cbde57000bfa9a3de5264357ad09e713c3
Lesaje <olehchaika.prop@gmail.com>
2022-11-17 10:23:02 +0200

    1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Days with bug: 17</h1>
+        <h1>Days with bug: 18</h1>
         <h2>(since 30.10.2022)</h2>
       </header>
     </div>
