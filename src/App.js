e95e4f320b8c6fb8f7af5c207de7866f78c1ee8a
Lesaje <olehchaika.prop@gmail.com>
2022-11-17 11:19:09 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import moment from "moment";
 
 function App() {
+    let time = moment("20221030", "YYYYMMDD").fromNow()
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Days with bug: 18</h1>
+        <h1>Bug was found {time}</h1>
         <h2>(since 30.10.2022)</h2>
       </header>
     </div>
